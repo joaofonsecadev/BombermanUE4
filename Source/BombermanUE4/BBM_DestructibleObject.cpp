a9114f46490e797c31e8b88aa388fcdcc8640035
João Fonseca <joaofvfonseca+github@gmail.com>
2020-04-01 11:12:22 +0100

    Created base class for interface

---
+++ Source/BombermanUE4/BBM_DestructibleObject.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BBM_DestructibleObject.h"
+
+// Add default functionality here for any IBBM_DestructibleObject functions that are not pure virtual.
---
+++ Source/BombermanUE4/BBM_DestructibleObject.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "BBM_DestructibleObject.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class UBBM_DestructibleObject : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class BOMBERMANUE4_API IBBM_DestructibleObject
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+};
