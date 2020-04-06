4bef4131eada54817ee40758d98f51c85e65df60
IvoPenedo29 <ivoti.98@sapo.pt>
2020-04-06 13:58:29 +0100

    Checking collision radius

--- Source/BombermanUE4/BBM_Bomb.cpp
+++ Source/BombermanUE4/BBM_Bomb.cpp
@@ -32,6 +32,25 @@ void ABBM_Bomb::Tick(float DeltaTime)
 
 void ABBM_Bomb::Explode()
 {
-	DrawDebugSphere(GetWorld(), GetActorLocation(), ExplosionRadius, 25.0f, FColor::Cyan, false, 1.0f, 0, 2.0f);
+	//DrawDebugSphere(GetWorld(), GetActorLocation(), ExplosionRadius, 25.0f, FColor::Cyan, false, 1.0f, 0, 2.0f);
+	
+	TArray<FHitResult> OutHits;
+	
+	FVector ActorLocation = GetActorLocation();
+
+	FCollisionShape CollisionSphere = FCollisionShape::MakeSphere(ExplosionRadius);
+
+	DrawDebugSphere(GetWorld(), ActorLocation, CollisionSphere.GetSphereRadius(), 10, FColor::Purple, true);
+
+	bool bIsHit = GetWorld()->SweepMultiByChannel(OutHits, ActorLocation, ActorLocation, FQuat::Identity, ECC_WorldStatic, CollisionSphere);
+
+	if (bIsHit) 
+	{
+		for (auto& Hit : OutHits) 
+		{		
+			UE_LOG(LogTemp, Error, TEXT("It hit: %s"), *Hit.Actor->GetName());
+		}
+	}
+
 	Destroy();
 }
