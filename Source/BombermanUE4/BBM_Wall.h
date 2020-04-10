746426b247168f414809fb8494cbeab1c600d7f5
IvoPenedo29 <ivoti.98@sapo.pt>
2020-04-10 13:55:54 +0100

    Recreated the wall class, and walls being destroyed by bombs

---
+++ Source/BombermanUE4/BBM_Wall.h
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BBM_DestructibleObject.h"
+#include "BBM_Wall.generated.h"
+
+UENUM()
+enum class PowerUpType : uint8
+{
+	MoreBombs,
+	KickBombs
+};
+
+UCLASS()
+class BOMBERMANUE4_API ABBM_Wall : public AActor, public IBBM_DestructibleObject
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABBM_Wall();
+
+	virtual void DestroySelf() override;
+	
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<AActor> KickBombsPowerUp;
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<AActor> MoreBombsPowerUp;
+
+	UPROPERTY(VisibleAnywhere)
+	PowerUpType Type;
+
+	bool bHasPowerUp = false;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	void SetPowerUpType();
+	void SpawnPowerUp();
+};
