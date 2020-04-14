b38cf15869a30301704e3065f17248bd0b9979dc
João Fonseca <joaofvfonseca+github@gmail.com>
2020-04-14 19:55:04 +0100

    MainMenu class with default constructor

---
+++ Source/BombermanUE4/BBM_UI_MainMenu.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BBM_UI_MainMenu.h"
+
+UBBM_UI_MainMenu::UBBM_UI_MainMenu(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+
+}
---
+++ Source/BombermanUE4/BBM_UI_MainMenu.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "BBM_UI_MainMenu.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BOMBERMANUE4_API UBBM_UI_MainMenu : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	UBBM_UI_MainMenu(const FObjectInitializer& ObjectInitializer);
+};
