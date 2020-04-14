eb1086ccaf9e6d49f58f84aaf7ea40ce7d894985
João Fonseca <joaofvfonseca+github@gmail.com>
2020-04-14 22:27:03 +0100

    Created ConnectToIP class with default constructor

---
+++ Source/BombermanUE4/BBM_UI_ConnectToIP.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BBM_UI_ConnectToIP.h"
+
+UBBM_UI_ConnectToIP::UBBM_UI_ConnectToIP(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+
+}
---
+++ Source/BombermanUE4/BBM_UI_ConnectToIP.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "BBM_UI_ConnectToIP.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BOMBERMANUE4_API UBBM_UI_ConnectToIP : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	UBBM_UI_ConnectToIP(const FObjectInitializer& ObjectInitializer);
+};
